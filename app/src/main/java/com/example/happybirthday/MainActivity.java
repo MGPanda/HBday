3cabe68a6ede500454c15ff9fc641b17fb228db6
Cristóbal Martínez <wipeoman@gmail.com>
2019-10-10 18:19:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/happybirthday/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.happybirthday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private final TextView text = findViewById(R.id.textView);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+        final Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+                                 public void onClick(View v) {
+                                     text.setText("El primer memillo");
+                                 }
+                             }
+        );
+        final Button b2 = findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+                                 public void onClick(View v) {
+                                     text.setText("El segundo memillo");
+                                 }
+                             }
+        );
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+}
